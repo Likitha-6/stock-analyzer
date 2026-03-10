4127152f7554ba287229d1db746521a7585744e3
Likitha-6 <likitharouthu@gmail.com>
2026-03-10 09:27:09 -0400

    Refactor data loading functions in data.py
    Removed redundant load_master function and adjusted load_name_lookup.

--- common/data.py
+++ common/data.py
@@ -1,10 +1,5 @@
-
 import pandas as pd
 import streamlit as st
-
-@st.cache_data(ttl=60*60*6)
-def load_master():
-    return pd.read_csv("data/nse_stocks_with industries.csv")
 
 @st.cache_data(ttl=60*60*6)
 def load_name_lookup():
